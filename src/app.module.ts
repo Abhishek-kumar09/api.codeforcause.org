a09d347662314a59d3de10e5f3515f26de3608e1
KeenWarrior <anujgargcse@gmail.com>
2020-06-04 23:46:28 +0530

    Add cors permission with app variable

--- src/app.module.ts
+++ src/app.module.ts
@@ -11,7 +11,7 @@ import {
     KeycloakModule.register({
       authServerUrl: 'https://authbox.codeforcause.org/auth/',
       realm: 'development',
-      clientId: 'dev-serve',
+      clientId: 'dev-server',
       secret: '982830cc-6230-457b-869c-51045b956f8c'
     })
   ],
